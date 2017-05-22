9414988412fab583e305206494e591d6c11468f4
Stephen Stewart <stephen.stewart@carisenda.com>
2017-05-22 15:03:33 +0100

    demonstrate what shallow render allows

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export class Foo extends Component {
+  render() {
+    return (
+      <h3>Shallow rendering test</h3>
+    )
+  }
+}
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <Foo />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,5 +26,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App, { Foo } from './App';
 
+// the shallow smoke test from create-react-app docs
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('no child components', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  const testme = <Foo />;
+
+  // shallow rendering limits you to asserts against the component in question
+  // only, not it's children
+  expect(wrapper.contains(welcome)).toEqual(true);
+  // you can assert that the component under test has a direct child
+  expect(wrapper.contains(testme)).toEqual(true);
+  // but you can't assert what's in that child
+  expect(wrapper.find('h3').length).toEqual(0);
+  // to do that you can .dive()
+  expect(wrapper.find(Foo).dive().find('h3').length).toEqual(1);
 });
